193b40c94b2d19cbfe8a2cc32ef9005d153240a9
Amadej Pevec <amadej.pevec@gmail.com>
2023-08-09 15:09:06 +0200

    feat: add support for merging pages into one pdf

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const http = require("https");
 const fs = require("fs");
 const { PDFDocument } = require("pdf-lib");
+const axios = require("axios");
 
 const token = "token";
 const filename = "SLOINJAZ_SLO1_Celota_SS_SDZ";
-const lastPage = 216;
 
 const prepareTempFolder = (filename) => {
   if (!fs.existsSync(filename)) {
@@ -17,7 +17,8 @@ const timeout = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 const merge = async () => {
   const folderName = `${filename}merged`;
   prepareTempFolder(folderName);
-  for (let i = 1; i <= lastPage; i++) {
+  const pageCount = await getPageCount();
+  for (let i = 1; i <= pageCount; i++) {
     const pageBytes = fs.readFileSync(`./${filename}pdf/${i}.pdf`);
     const answersBytes = fs.readFileSync(`./${filename}answers/${i}.pdf`);
     const pdfDoc = await PDFDocument.load(pageBytes);
@@ -30,32 +31,62 @@ const merge = async () => {
   }
 };
 
+const mergePages = async (type) => {
+  const folderName = `${filename}${type}`;
+  const fileNames = fs
+    .readdirSync(folderName, { withFileTypes: true })
+    .filter((file) => file.name.endsWith(".pdf"))
+    .sort((a, b) => parseInt(a.name.replace(".pdf", "")) - parseInt(b.name.replace(".pdf", "")));
+  const merged = await PDFDocument.create();
+
+  for (const fileName of fileNames) {
+    const fileBytes = fs.readFileSync(`./${folderName}/${fileName.name}`);
+    const file = await PDFDocument.load(fileBytes);
+    const pages = await merged.copyPages(file, file.getPageIndices());
+    pages.forEach((page) => merged.addPage(page));
+  }
+
+  const mergedPdfFile = await merged.save();
+  fs.writeFileSync(`./${folderName}/merged${type}.pdf`, mergedPdfFile);
+};
+
 const scrape = async (type) => {
   const folderName = `${filename}${type}`;
   prepareTempFolder(folderName);
-  for (let i = 1; i <= lastPage; i++) {
+  const pageCount = await getPageCount();
+  for (let i = 1; i <= pageCount; i++) {
     const file = fs.createWriteStream(`${folderName}/${i}.pdf`);
     http.get(`https://api-folio.rokus-klett.si/v1/${type}/${filename}/${i}?token=${token}`, (response) =>
       response.pipe(file)
     );
 
     if (i % 25 === 0) {
+      console.log(`Scraping ${i} from ${pageCount}`);
       await timeout(25000);
     }
   }
 };
 
+const getPageCount = async () => {
+  const res = await axios.get(`https://api-folio.rokus-klett.si/v1/metadata/${filename}`);
+  return res.data.pageCount;
+};
+
 (async () => {
   const option = process.argv[2];
+  const type = process.argv[3];
 
   switch (option) {
     case "scrape":
-      const type = process.argv[3];
       await scrape(type);
       console.log("scraping finished");
       break;
     case "merge":
       await merge();
+      console.log("merging finished");
+      break;
+    case "merge-pages":
+      await mergePages(type);
       console.log("merging finished");
       break;
     default:
